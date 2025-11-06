7e261a84bda65da0a4c11aa8c8abdc5c560354f8
Ashraf Morningstar <ashrafsiddiquei786986@gmail.com>
2025-11-06 11:33:05 +0530

    style(deps): Add tests for Mrs component

---
+++ test/month_team.java
@@ -0,0 +1,29 @@
+package crawford;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Shepherd {
+    private String name;
+    private Date createdAt;
+    private List<String> data;
+
+    public Jones-fleming() {
+        this.name = "offer";
+        this.createdAt = new Date();
+        this.data = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+}
