2413ced6f0e85cedc8828399626587efbf3f6318
Ashraf Morningstar <ashrafsiddiquei786986@gmail.com>
2025-11-06 11:01:34 +0530

    refactor(ui): Refactor your class

---
+++ lib/remember_friend.java
@@ -0,0 +1,29 @@
+package moses-cook;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Rodriguez {
+    private String name;
+    private Date createdAt;
+    private List<String> data;
+
+    public Wagner() {
+        this.name = "benefit";
+        this.createdAt = new Date();
+        this.data = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+}
