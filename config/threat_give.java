ecfa16013005054369a851c22052f79d6d0b61de
Ashraf Morningstar <ashrafsiddiquei786986@gmail.com>
2025-11-06 11:05:32 +0530

    chore(auth): Improve suggest performance

---
+++ config/threat_give.java
@@ -0,0 +1,29 @@
+package simmons;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Martin {
+    private String name;
+    private Date createdAt;
+    private List<String> data;
+
+    public Johns() {
+        this.name = "officer";
+        this.createdAt = new Date();
+        this.data = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+}
